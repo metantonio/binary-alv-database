3157243d9f65f0558dc334d24cf0afdf4c70519d
unknown <antonio_martinez88@hotmail.com>
2024-07-11 13:52:45 -0400

    updated

--- check_database.py
+++ check_database.py
@@ -5,20 +5,21 @@ if __name__ == '__main__':
 
     # Load the database from binary format
     loaded_db = CustomDatabase.load('./avl_database.pkl')
-    print("\nLoaded database user with id 0:", loaded_db.select('users', 2))
+    row_header, user_row = loaded_db.select('users', 0)
+    print("\nPrint user with id 0:\n", row_header, '\n', user_row)
 
     # Insert new data
     loaded_db.insert('users', [None, 'Antonio', 35])
     loaded_db.commit('users')
-    print("Inserted new user in users")
+    print("\nInserted new user in users")
 
     # Select by value
     header, user = loaded_db.select_by_column_value("users", "age", '>', 25)
-    print("\nSelect user row:", header, user)
+    print("\nSelect user row:\n", header,'\n', user)
 
     # Select all data
     headers, table = loaded_db.select_all('users')
-    print("\nSelect all users table:", headers, table)
+    print("\nSelect all users table:\n", headers, '\n', table)
 
     # AFTER COMMITS ARE MADE, WRITE TO THE DATABASE
     loaded_db.save('./avl_database.pkl')
