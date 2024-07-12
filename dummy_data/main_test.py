7262c26de7447abe5e433afe4fd1966318dbbe78
unknown <antonio_martinez88@hotmail.com>
2024-07-12 10:09:13 -0400

    updated

--- dummy_data/main_test.py
+++ dummy_data/main_test.py
@@ -6,12 +6,36 @@ from ..databasestructure import *
 if __name__ == '__main__':
     try:
         db = CustomDatabase()
+        
+        # Create a tables 
         start_time = time.time()
-        # Create a table with the 'id' as the key_index start=0
         db.create_table('users', ['id', 'name', 'age'], key_index=0)
-        print("table created")
+        db.create_table('orders', ['order_id', 'user_id', 'product'], key_index=0)
+        end_time = time.time()
+        total_time = end_time - start_time
+        print(f"Tables creation, Total execution time: {total_time} seconds\n")
 
+        # Load data from CSV files
+        start_time = time.time()
         db.load_from_csv("users", "./dummy_users.csv")
+        db.commit('users')
+        end_time = time.time()
+        total_time = end_time - start_time
+        print(f"Loaded 10,000 users rows to memory, Total execution time: {total_time} seconds\n")
+
+        start_time = time.time()
+        db.load_from_csv("orders", "./dummy_orders.csv")
+        db.commit('orders')
+        end_time = time.time()
+        total_time = end_time - start_time
+        print(f"Loaded 20,000 orders rows to memory, Total execution time: {total_time} seconds\n")
+
+        # I/O Write binary database to disk
+        start_time = time.time()
+        db.save('./dummy_database.pkl')
+        end_time = time.time()
+        total_time = end_time - start_time
+        print(f"Loaded 20,000 orders rows to memory, Total execution time: {total_time} seconds\n")
 
     except Exception as error:
         print(str(error))
