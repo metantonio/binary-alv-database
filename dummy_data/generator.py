9c9dcb1eb96c51da04a62f728a5e23eae59c78a0
unknown <antonio_martinez88@hotmail.com>
2024-07-12 09:55:05 -0400

    updated

--- dummy_data/generator.py
+++ dummy_data/generator.py
@@ -1,12 +1,19 @@
 import csv
 import random
 
+# Users dummy data variables
 path_csv = "./dummy_data/"
 file_name='dummy_users.csv'
 file_path = path_csv + file_name
 num_users = 10000
 min_age = 18
 max_age = 60
+
+# Orders dummy data variable
+path_csv2 = "./dummy_data/"
+file_name2='dummy_orders.csv'
+file_path2 = path_csv2 + file_name2
+products = ["apple", "laptop", "smartphone", "watch", "tv", "pear"]
 
 # Abrir el archivo CSV para escribir
 with open(file_path, mode='w', newline='') as file:
@@ -21,4 +28,19 @@ with open(file_path, mode='w', newline='') as file:
         age = random.randint(min_age, max_age)  # Generar una edad aleatoria entre 18 y 90
         writer.writerow([user_id, name, age])
 
-print("File generated")
+print("File 1 generated")
+
+# Abrir el archivo CSV para escribir
+with open(file_path2, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    
+    # Escribir la fila de encabezado
+    writer.writerow(['order_id', 'user_id', 'product'])
+    
+    # Generar y escribir los datos de los usuarios
+    for order_id in range(1, num_users + 1):
+        product = random.choice(products)
+        user_id = random.randint(1, num_users)  # Generar una edad aleatoria entre 18 y 90
+        writer.writerow([order_id, user_id, product])
+
+print("File 2 generated")
